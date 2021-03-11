d9beb9dd1eac0bed91bb181dba6855502e16cd01
YunFei-Bai <baiyunfei@example.com>
2021-03-11 10:56:00 +0800

    无

--- stl.py
+++ stl.py
@@ -8,20 +8,9 @@ import plotly.io as pio
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib; matplotlib.use('TkAgg')
-import statsmodels.api as sm
-from statsmodels.tsa.seasonal import seasonal_decompose
-data = pd.read_csv("kpi_0129.csv")
-tdata = data['mrt']
-rd = sm.tsa.seasonal_decompose(tdata.values, period=11)
-length = len(tdata)
-print(rd.seasonal, rd.trend, rd.resid)
-# shared_xaxes 就可以实现同时控制多个子图的功能
+data = pd.read_csv("kpi_0304.csv")
+data1 = int(data['timestamp'])
+for i in range(len(data1)-1):
+    if data[i+1] - data[i] > 60:
+        print(i)
 
-fig = make_subplots(rows=4, cols=1, shared_xaxes=True)
-fig.add_trace(go.Scatter(x=np.arange(0, length), y=tdata, mode='lines', name='原始数据', marker={"line": {"color": "blue"}}), row=1, col=1)
-fig.add_trace(go.Scatter(x=np.arange(0, length), y=rd.seasonal, mode='lines', name='季节项', marker={"line": {"color": "red"}}), row=2, col=1)
-fig.add_trace(go.Scatter(x=np.arange(0, length), y=rd.trend, mode='lines', name='趋势项', marker={"line": {"color": "green"}}), row=3, col=1)
-fig.add_trace(go.Scatter(x=np.arange(0, length), y=rd.resid, mode='lines', name='残差项', marker={"line": {"color": "black"}}), row=4, col=1)
-fig_html = "./" + 'ssss' + ".html"
-pio.write_html(fig, file=fig_html)
-fig.show()
